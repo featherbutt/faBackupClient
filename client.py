efc5371a99b58fdcc65098eece58b02c18d15cf0
Featherbutt <iamjohndoe220@gmail.com>
2024-08-29 15:37:50 -0700

    Initial commit

---
+++ client.py
@@ -0,0 +1,144 @@
+# In a loop:
+# Call nextBatch() to get the next batch of data
+# Run FALR to create a local DB
+# Upload it
+
+import sys
+import requests
+import os
+import string
+import random
+import subprocess
+import sqlite3
+import time
+
+_, hostName, secret, a, b, batchSize = sys.argv
+
+headers = {'Authorization': f"Bearer {secret}"}
+
+def get(url: str):
+    response = requests.get(url, headers=headers)
+    while response.status_code != 200:
+        print(f"Failed to get {url}, sleeping for 30 seconds")
+        time.sleep(30)
+        response = requests.get(url, headers=headers)
+
+    return response.json()
+
+def post(url: str, data: dict):
+    response = requests.post(url, data=data, headers=headers)
+    while response.status_code != 200:
+        print(f"Failed to post {url}, sleeping for 30 seconds")
+        time.sleep(30)
+        response = requests.post(url, data=data, headers=headers)
+
+
+def gen_rand_char() -> str:
+    char_set = string.ascii_uppercase + string.digits
+    return (''.join(random.sample(char_set*6, 6)))
+
+def get_last_id(dbName):
+    db = sqlite3.connect(dbName)
+    cursor = db.cursor()
+    cursor.execute('''
+        SELECT id FROM submissions order by id desc limit 1;
+    ''')
+    ids = cursor.fetchall()
+    db.close()
+    if len(ids) == 0:
+        return None
+    return ids[0][0]
+
+def initDb(dbName: str):
+    subprocess.run(['falocalrepo', 'init', '--database', dbName])
+    subprocess.run(['falocalrepo', 'config', 'cookies', '--database', dbName, '-c', 'a', a, '-c', 'b', b])
+
+def uploadSubmissions(ids: list[int]):
+    dbName = f'./dbs/FA_list_{gen_rand_char()}.db'
+
+    initDb(dbName)
+
+    RETRIES = 2
+    for i in range(RETRIES):
+        result = subprocess.run(['falocalrepo', 'download', 'submissions', '--database', dbName, *(str(i) for i in ids)])
+        if result.returncode == 0:
+            break
+        newMax = get_last_id(dbName)
+        if newMax is None:
+            ids = (id for id in ids if id > newMax)
+    else:
+        print(f"Failed to download list")
+        return False
+    
+    with open(dbName, 'rb') as f:
+        post(f'https://{hostName}/upload/list/', data=f)
+        print("Uploaded list")
+
+def uploadRange(min, max):
+
+    dbName = f'./dbs/FA_{min}_{max}_{gen_rand_char()}.db'
+
+    initDb(dbName)
+    
+    newMax = max
+    RETRIES = 2
+    for i in range(RETRIES):
+        result = subprocess.run(['falocalrepo', 'download', 'range', '--database', dbName, min, newMax])
+        print(result.stderr)
+        print(result.stdout)
+        if result.returncode == 0:
+            break
+        
+        newMax = get_last_id(dbName)
+        if newMax is None:
+            newMax = max
+    else:
+        print(f"Failed to download {min} to {max}")
+        return False
+    
+    with open(dbName, 'rb') as f:
+        post(f'https://{hostName}/upload/range/{min}/{max}', data=f)
+        print("Uploaded range {min} to {max}")
+    
+
+def uploadArtist(artist):
+    # artists_str = '_'.join(artists)
+    dbName = f'./dbs/FA_{artist}_{gen_rand_char()}.db'
+
+    initDb(dbName)
+
+    #artist_tags = [tag for artist in artists for tag in ['-u', artist]]
+    RETRIES = 2
+    for i in range(RETRIES):
+        result = subprocess.run(['falocalrepo', 'download', 'users', '-u', artist,
+                                 '-f', 'gallery', '-f', 'scraps', '-f', 'journals', '-f', 'userpage', '--database', dbName])
+        if result.returncode == 0:
+            break
+        newMax = get_last_id(dbName)
+        if newMax is None:
+            newMax = artist
+    else:
+        print(f"Failed to download artist {artist}")
+        return False
+    
+    with open(dbName, 'rb') as f:
+        post(f'https://{hostName}/upload/artist/{artist}', data=f)
+        print(f"Uploaded artist {artist}")
+    
+    
+while True:
+    nextBatch = get(f'https://{hostName}/nextBatch/{batchSize}')
+    if nextBatch["done"]:
+        break
+    
+    print(nextBatch)
+    match nextBatch["type"]:
+        case "list":
+            uploadSubmissions(nextBatch["ids"])
+        case "range":
+            uploadRange(str(nextBatch["min"]), str(nextBatch["max"]))
+        case "artist":
+            uploadArtist(nextBatch["artist"])
+        case _:
+            print("Unknown batch type")
+            break
---
+++ run.py
@@ -0,0 +1,33 @@
+import argparse
+import subprocess
+
+parser = argparse.ArgumentParser(
+                    prog='Furaffinity Backup Client',
+                    description='Creates a backup of Furaffinity.')
+
+parser.add_argument('--dbDir', type=str, help='Directory to store databases', required=True)
+parser.add_argument('--url', type=str, help='Server URL', required=True)
+parser.add_argument('--secret', type=str, help='Server authorization token', required=True)
+parser.add_argument('-a', type=str, help='Furaffinity cookie "a"', required=True)
+parser.add_argument('-b', type=str, help='Furaffinity cookie "b"', required=True)
+
+parser.add_argument('--batchSize', type=int, default=1000, help='Batch size for submissions')
+
+args = parser.parse_args()
+
+# Parse command line args
+# Initialize submodules
+
+subprocess.run(['git', 'submodule', 'init'])
+subprocess.run(['git', 'submodule', 'update'])
+
+# Build the dockerfile if it doesn't exist
+
+subprocess.run(['docker', 'build', '-t', 'fascraper', '.'])
+
+# Run the dockerfile
+
+subprocess.run(['docker', 'run',
+                '-v', f'{args.dbDir}:/app/dbs',
+                'fascraper',
+                'python', 'client.py', args.url, args.secret, args.a, args.b, args.batchSize])
