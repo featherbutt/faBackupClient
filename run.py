e324d5bd2e0997820c451b704c49c45e36fcd8a0
Featherbutt <iamjohndoe220@gmail.com>
2024-08-29 16:54:50 -0700

    Add ability to forward environment variables to client

--- run.py
+++ run.py
@@ -2,7 +2,6 @@ import argparse
 import subprocess
 
 parser = argparse.ArgumentParser(
-                    prog='run.py',
                     description='Furaffinity Backup Client Runner')
 
 parser.add_argument('--dbDir', type=str, help='Directory to store databases', required=True)
@@ -12,6 +11,8 @@ parser.add_argument('-a', type=str, help='Furaffinity cookie "a"', required=True
 parser.add_argument('-b', type=str, help='Furaffinity cookie "b"', required=True)
 
 parser.add_argument('--batchSize', type=int, default=1000, help='Batch size for submissions')
+
+parser.add_argument('-e', '--env', type=str, nargs="*", help='additional environment variables')
 
 args = parser.parse_args()
 
@@ -27,7 +28,10 @@ subprocess.run(['docker', 'build', '-t', 'fascraper', '.'])
 
 # Run the dockerfile
 
+env_args = [arg for env in args.env for arg in ['-e', env]]
+
 subprocess.run(['docker', 'run',
                 '-v', f'{args.dbDir}:/app/dbs',
+                *env_args,
                 'fascraper',
                 'python', 'client.py', args.url, args.secret, args.a, args.b, str(args.batchSize)])
